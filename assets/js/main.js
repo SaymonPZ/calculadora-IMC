e7c701761a2f8490e4ca1f519fba416f33162aa3
Saymon <saymon.espindola@gmail.com>
2024-04-25 10:15:00 -0300

    base pronta

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,16 +15,27 @@ let calculatorIMC = {
         this.draw(result.toFixed(2));
     },
     draw(imcResult){
+        const colorResults = document.querySelector('.colorResults');
+        const printResult = document.querySelector('.resultado');
+
         if(imcResult < 18.5){
-            console.log(`Resultado: ${imcResult}, você está abaixo do peso.`);
+           printResult.innerText = `Resultado: ${imcResult}, você está abaixo do peso.`;
+           colorResults.style = 'background-color: #80D8FF';
         } else if(imcResult >= 18.5 && imcResult < 25){
-            console.log(`Resultado: ${imcResult}, você está com peso normal.`);
+            printResult.innerText = `Resultado: ${imcResult}. Você está com peso normal.`;
+            colorResults.style = 'background-color: #AED581';
         } else if(imcResult >= 25 && imcResult < 29.9){
-            console.log(`Resultado: ${imcResult}, você está com sobrepeso.`);
+           printResult.innerText = `Resultado: ${imcResult}, você está com sobrepeso.`;
+           colorResults.style = 'background-color: #FFB74D';
         } else if(imcResult >= 29.9 && imcResult < 39.9){
-            console.log(`Resultado: ${imcResult}, você está com obsidade.`);
+           printResult.innerText = `Resultado: ${imcResult}, você está com obsidade.`;
+           colorResults.style = 'background-color: #FF7043';
+        } else if(imcResult >= 39.9){
+           printResult.innerText = `Resultado: ${imcResult}, você está com obsidade grave`;
+           colorResults.style = 'background-color: #E53935';
         } else{
-            console.log(`Resultado: ${imcResult}, você está com obsidade grave`);
+            printResult.innerText = `Insira valores validos`;
+            colorResults.style = 'background-color: #fff';
         }
     }
 }
