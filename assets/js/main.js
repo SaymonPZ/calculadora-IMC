d725ddf8a7cc4d92f94edd03756f67d64c546ae6
Saymon <saymon.espindola@gmail.com>
2024-04-25 10:24:33 -0300

    infinity results

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ let calculatorIMC = {
         const colorResults = document.querySelector('.colorResults');
         const printResult = document.querySelector('.resultado');
 
-        if(imcResult < 18.5){
+        if(imcResult > 0 && imcResult < 18.5){
            printResult.innerText = `Resultado: ${imcResult}, você está abaixo do peso.`;
            colorResults.style = 'background-color: #80D8FF';
         } else if(imcResult >= 18.5 && imcResult < 25){
@@ -30,7 +30,7 @@ let calculatorIMC = {
         } else if(imcResult >= 29.9 && imcResult < 39.9){
            printResult.innerText = `Resultado: ${imcResult}, você está com obsidade.`;
            colorResults.style = 'background-color: #FF7043';
-        } else if(imcResult >= 39.9){
+        } else if(imcResult >= 39.9 && imcResult != Infinity){
            printResult.innerText = `Resultado: ${imcResult}, você está com obsidade grave`;
            colorResults.style = 'background-color: #E53935';
         } else{
